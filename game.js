91d2188a9211f9705588f2a088a20217daad9c2e
wikuuu1 <wiktoria.rajba391@gmail.com>
2022-01-11 21:42:48 +0100

    połykSound added, "start game" button added with js function 'startGame()'

--- game.js
+++ game.js
@@ -1,6 +1,20 @@
+const polykSound = new Audio("polykSound.mp3");
+
 initGame();
 
 function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
+}
+
+function play(){
+    polykSound;
+    polykSound.play();
+}
+
+function startGame() {
+    let startDiv = document.getElementById("start");
+    startDiv.style.display = "none";
+
+    start();
 }
