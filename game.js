33b677ac5c3dfec91b95d4c2f5f9aed03cfe2b7a
wikuuu1 <wiktoria.rajba391@gmail.com>
2022-01-13 19:26:36 +0100

    added function changeSnakeDirection(x, y) for not allow snek to go outside the gameboard

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 const polykSound = new Audio("polykSound.mp3");
+let direction = "d";
+var intervalId;
 
-let direction = "d"
 initGame();
 
 function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
-    setInterval(moveSneak, 500)
+    intervalId = setInterval(moveSneak, 500);
     window.addEventListener('keydown', (edge)=>{
         console.log(edge.key)
         if (edge.key === "d") {
@@ -36,21 +37,32 @@ function moveSneak() {
     let sneak = document.getElementById('sneak');
     let y = getComputedStyle(sneak).getPropertyValue('grid-column-start');
     let x = getComputedStyle(sneak).getPropertyValue('grid-row-start');
-    
-    if (direction === "d") {
+   
+    changeSnakeDirection(x, y);
+
+}
+
+function changeSnakeDirection(x, y){
+
+    if (direction === "d" && parseInt(y) != 20) { 
         sneak.style.gridColumnStart = (parseInt(y) + 1);
         document.querySelector("#sneak").style.transform = "rotate(0deg)";
     }
-    if (direction === "a") {
+    else if (direction === "a" && parseInt(y) != 1)  {
         sneak.style.gridColumnStart = (parseInt(y) - 1);
         document.querySelector("#sneak").style.transform = "rotate(180deg)";
     }
-    if (direction === "w") {
+    else if (direction === "w" &&  parseInt(x) != 1)  {
         sneak.style.gridRowStart = (parseInt(x) - 1);
         document.querySelector("#sneak").style.transform = "rotate(270deg)";
     }
-    if (direction === "s") {
+    else if (direction === "s" && parseInt(x) != 20) {
         sneak.style.gridRowStart = (parseInt(x) + 1);
         document.querySelector("#sneak").style.transform = "rotate(90deg)";
+    } 
+    else {
+        clearInterval(intervalId);
     }
-}
+   
+          
+    }
